847a1af5165fb9297e534d297eeaf262989e2adb
Francois Janson <jansonfrancois@gmail.com>
2021-08-23 14:07:36 +0200

    creating components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Children } from "react";
+import Add from "./components/Add";
+import List from "./components/List";
+import Pay from "./components/Pay";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "add",
+      items: [],
+    };
+  }
+
+  handleSubmit = () => {};
+
+  render() {
+    return (
+      <>
+        <h1 className="text-center mt-5">Bakery</h1>
+        <Button onClick="" isSelected={activeTab === children ? true : false}>
+          Add
+        </Button>
+        <Button onClick="" isSelected={activeTab === children ? true : false}>
+          List
+        </Button>
+        <Button onClick="" isSelected={activeTab === children ? true : false}>
+          Pay
+        </Button>
+        <Add />
+        <List />
+        <Pay />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Pay</h1>
+      </>
+    );
+  }
+}
+
+export default Button;
