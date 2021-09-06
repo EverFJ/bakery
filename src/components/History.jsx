49d44b12c83a0eb1cd9970527b642e860a4f33a9
Francois Janson <jansonfrancois@gmail.com>
2021-09-06 15:48:20 +0200

    cleaning code

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -3,9 +3,17 @@ import React from "react";
 class History extends React.Component {
   render() {
     return (
-      <>
-        <h1>History</h1>
-      </>
+      <div className="container m-5">
+        <div className="row">
+          <ul className="list-group">
+            {this.props.items.map((elem) => (
+              <li className="list-group-item col-md-6 col-12">
+                {elem.name} : {elem.price}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
